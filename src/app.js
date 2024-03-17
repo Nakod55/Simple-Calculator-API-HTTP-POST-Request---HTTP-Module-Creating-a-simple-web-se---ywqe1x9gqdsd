a830fa05e0dcaab23aac1c3e533950392d0d1d16
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-17 23:43:27 +0530

    submit

--- src/app.js
+++ src/app.js
@@ -8,21 +8,136 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const min=-1000000;
+const max=1000000;
 //Write POST endpoint to get the sum of two number
-
+app.post("/add",(req,res)=>{
+  const num1=Number(req.body.num1);
+  const num2=Number(req.body.num2);
+  console.log(num1,num2);
+  if(isNaN(num1)||isNaN(num2))
+  {
+    res.send({
+      status: "error",
+      message: "Invalid data types"
+    })
+  }
+  const ans=num1+num2;
+  if(num1<min||num2<min||ans<min)
+  {
+    res.send({
+      status:"error",
+      message:"Underflow"
+    })
+  }
+  if(num1>max||num2>max||ans>max)
+  {
+    res.send({
+      status:"error",
+      message:"Overflow"
+    })
+  }
+  res.send({result: ans});
+})
 
 //Write POST endpoint to get the differance of two number
-
+app.post("/subtract",(req,res)=>{
+  const num1=Number(req.body.num1);
+  const num2=Number(req.body.num2);
+  if(isNaN(num1)||isNaN(num2))
+  {
+    res.send({
+      status: "error",
+      message: "Invalid data types"
+    })
+  }
+  const ans=num1-num2;
+  if(num1<min||num2<min||ans<min)
+  {
+    res.send({
+      status:"error",
+      message:"Underflow"
+    })
+  }
+  if(num1>max||num2>max||ans>max)
+  {
+    res.send({
+      status:"error",
+      message:"Overflow"
+    })
+  }
+  res.send({result: ans});
+})
 
 //Write POST endpoint to get the multiplication of two number
-
+app.post("/multiply",(req,res)=>{
+  const num1=Number(req.body.num1);
+  const num2=Number(req.body.num2);
+  if(isNaN(num1)||isNaN(num2))
+  {
+    res.send({
+      status: "error",
+      message: "Invalid data types"
+    })
+  }
+  const ans=num1*num2;
+  if(num1<min||num2<min||ans<min)
+  {
+    res.send({
+      status:"error",
+      message:"Underflow"
+    })
+  }
+  if(num1>max||num2>max||ans>max)
+  {
+    res.send({
+      status:"error",
+      message:"Overflow"
+    })
+  }
+  res.send({result: ans});
+})
 
 //Write POST endpoint to check if the num2 is 0 or not and to get the result after dividing two number
-
+app.post("/divide",(req,res)=>{
+  const num1=Number(req.body.num1);
+  const num2=Number(req.body.num2);
+  if(isNaN(num1)||isNaN(num2))
+  {
+    res.send({
+      status: "error",
+      message: "Invalid data types"
+    })
+  }
+  if(num2==0)
+  {
+    res.send({
+      status:"error",
+      message:"Cannot divide by zero"
+    })
+  }
+  const ans=num1/num2;
+  if(num1<min||num2<min||ans<min)
+  {
+    res.send({
+      status:"error",
+      message:"Underflow"
+    })
+  }
+  if(num1>max||num2>max||ans>max)
+  {
+    res.send({
+      status:"error",
+      message:"Overflow"
+    })
+  }
+  res.send({result: ans});
+})
       
 
 const server = app.listen(4000, () => {
   console.log(`Server running on port 4000`);
+  
 });
     
 module.exports = app;
